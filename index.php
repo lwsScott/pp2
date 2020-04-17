7e8a9a3d365dee66413ddcb7be26499c290b8e55
Lewis Scott <lscott19@mail.greenriver.edu>
2020-04-17 16:15:34 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Lewis Scott
+ * 4/17/20
+ * filename https://lscott.greenriverdev.com/328/pp2/index.php
+ * Pair Program 2
+ */
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
+<?php
+echo "PHP Array Practice";
