cd65d40355ad797ae4d9972d5d2e20b6a0d9abb4
Lewis Scott <lscott19@mail.greenriver.edu>
2020-04-19 10:40:03 -0400

    formatting and title

--- index.php
+++ index.php
@@ -9,22 +9,38 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 ?>
-<?php
-echo "PHP Array Practice" . "<br>";
-// Step 2
-echo "Step 2 - print array" . "<br>";
-$numbers = array(7, 9, 8, 8, 8, 6);
-// Step 3
-include ("functions.php");
-printArray($numbers);
-echo "Step 3 - moved function to a functions.php file" . "<br>";
-// Step 4
-echo "Step 4 - find largest number in array" . "<br>";
-largest($numbers);
-// Step 5
-echo "Step 5 - remove duplicates from array" . "<br>";
-removeDups($numbers);
-// Step 6
-echo "Step 6 - associative array with number and its frequency in array" . "<br>";
-distribution($numbers);
+    <!doctype html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Pair Program 2</title>
+    </head>
+    <body>
+    <?php
+
+        // Step 1
+        echo "PHP Array Practice" . "<br>";
+        // Step 2
+        echo "Step 2 - print array" . "<br>";
+        $numbers = array(7, 9, 8, 8, 8, 6);
+        // Step 3
+        include ("functions.php");
+        printArray($numbers);
+        echo "Step 3 - moved function to a functions.php file" . "<br>";
+        // Step 4
+        echo "Step 4 - find largest number in array" . "<br>";
+        largest($numbers);
+        // Step 5
+        echo "Step 5 - remove duplicates from array" . "<br>";
+        removeDups($numbers);
+        // Step 6
+        echo "Step 6 - associative array with number and its frequency in array" . "<br>";
+        distribution($numbers);
+
+    ?>
+    </body>
+    </html>
 
